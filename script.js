411394ba1dcfc29ac24a01f501fc5a1a82bfdba1
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-02-21 13:45:11 +0200

    help with 1st version of Pomodoro

--- script.js
+++ script.js
@@ -1,190 +1,67 @@
-let timerInterval;
 const timerDisplay = document.getElementById("timer");
 const actionButton = document.getElementById("actionButton");
 const nextButton = document.getElementById("nextButton");
 const stageDisplay = document.getElementById("stageDisplay");
 const cycleCountDisplay = document.getElementById("cycleCount");
 const restartButton = document.getElementById("restartButton");
-const soundAlertWork = new Audio("sounds/work.mp3");
-const soundAlertChill = new Audio("sounds/chill.mp3");
-const longChillFrequency = 3;
+const progressBar = document.querySelector(".e-c-progress");
+const pointer = document.getElementById("e-pointer");
+const volumeControl = document.getElementById("volumeControl");
+const settingsModal = document.getElementById("settingsModal");
+const settingsButton = document.getElementById("settingsButton");
+const span = document.getElementsByClassName("close")[0];
+const applySettingsButton = document.getElementById("applySettings");
 
+// Importing media files
+const soundAlertChillLong = new Audio("sounds/to_long_chill_notification.mp3");
+const soundAlertWork = new Audio("sounds/to_work_notification.mp3");
+const soundAlertChill = new Audio("sounds/to_chill_notification.mp3");
+const audio = new Audio("./sounds/music.mp3");
+
+// Deafult time config
 const durations = {
-  Work: 1 * 5,
-  Chill: 1 * 3,
-  "Long Chill": 1 * 10,
+  Work: 25 * 60,
+  Chill: 5 * 60,
+  "Long Chill": 15 * 60,
 };
 
-let remainingSeconds = durations["Work"];
-let isTimerRunning = false;
+// Deafult app setting
 let stage = "Work";
+let remainingSeconds = durations["Work"];
+let timerInterval;
+let longChillFrequency = 3;
+let volume = 0.5;
+let isTimerRunning = false;
 let cycleCount = 0;
 let workStagesCompleted = 0;
 let chillStagesCompleted = 0;
-let totalChillStages = 0;
-
-let progressBar = document.querySelector(".e-c-progress");
-let pointer = document.getElementById("e-pointer");
+let currentColor = "#FF5733";
+let musicPlaying = false;
 let length = Math.PI * 2 * 100;
 
+audio.volume = volume;
+audio.loop = true;
+
 progressBar.style.strokeDasharray = length;
-function addNewTaskForm() {
-  const form = document.createElement("div");
-  form.classList.add("task-modal");
-  const btns = document.createElement("div");
-  btns.classList.add("bnts-modal");
 
-  const heading = document.createElement("h2");
-  heading.classList.add("heading");
-  heading.textContent = "Task";
-
-  const input = document.createElement("input");
-  input.classList.add("task-modal-input");
-  const startButton = document.createElement("button");
-  startButton.classList.add("task-modal-button");
-
-
-
-  input.setAttribute("type", "text");
-  input.setAttribute("placeholder", "Введите название задания");
-  startButton.textContent = "Начать задание";
-  startButton.disabled = true;
-
-
-  // Add elements to form
-  form.appendChild(heading);
-  form.appendChild(input);
-  form.appendChild(startButton);
-
-  document.querySelector(".container").appendChild(form);
-  input.addEventListener("input", function () {
-    startButton.disabled = !input.value.trim();
-  });
-
-  // Обработчик событий для кнопки начала задания
-  startButton.addEventListener("click", function () {
-    const taskNameDisplay = document.createElement("div");
-    taskNameDisplay.classList.add("task-modal-div");
-    taskNameDisplay.textContent = `${input.value.trim()}`;
-    // Добавляем элемент с названием задачи в форму
-    form.appendChild(taskNameDisplay);
-    // Создаем кнопки завершения задания
-    const completeButton = document.createElement("button");
-    completeButton.classList.add("task-modal-button");
-    const notCompleteButton = document.createElement("button");
-    notCompleteButton.classList.add("task-modal-button");
-    // Настраиваем кнопки
-    completeButton.textContent = "complete";
-    notCompleteButton.textContent = "not complete";
-
-    // Добавляем кнопки на страницу
-    btns.appendChild(completeButton);
-    btns.appendChild(notCompleteButton);
-    form.appendChild(btns);
-
-    // Скрываем ввод и кнопку начала задания
-    input.style.display = "none";
-    startButton.style.display = "none";
-
-    // Обработчик для кнопки "complete"
-    completeButton.addEventListener("click", function () {
-      displayCompletionMessage(form, input.value, true);
-    });
-
-    // Обработчик для кнопки "not complete"
-    notCompleteButton.addEventListener("click", function () {
-      displayCompletionMessage(form, input.value, false);
-    });
-  });
+// Start initalization
+function initializeTimer() {
+  updateDisplay();
 }
-// Функция для отображения сообщения о завершении задания
-function displayCompletionMessage(form, taskName, isCompleted) {
-  const message = document.createElement("div");
-  message.classList.add("task-modal-div-complite");
-  message.textContent = `${taskName} was ${
-    isCompleted ? "completed" : "not completed"
-  }.`;
-  // Очищаем форму и отображаем сообщение
-  form.innerHTML = "";
-  form.appendChild(message);
+// Display prepering
+function updateDisplay() {
+  cangeTitle();
+  updateTimerDisplay();
+  remainingSeconds = durations[stage];
 }
-
-// Инициализация первой формы задания при загрузке страницы
-document.addEventListener("DOMContentLoaded", function () {
-  addNewTaskForm();
-});
-
-function updateCircle(value, timePercent) {
-  var offset = (-length * value) / timePercent;
-  progressBar.style.strokeDashoffset = offset;
-  pointer.style.transform = `rotate(${(360 * value) / timePercent}deg)`;
-}
-
+// Update timer
 function updateTimerDisplay() {
   const minutes = Math.floor(remainingSeconds / 60);
   const seconds = remainingSeconds % 60;
   timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
-function switchStage() {
-  if (stage === "Work") {
-    workStagesCompleted++;
-    if (
-      workStagesCompleted % 2 === 0 &&
-      chillStagesCompleted >= longChillFrequency
-    ) {
-      stage = "Long Chill";
-      chillStagesCompleted = 0;
-    } else {
-      stage = "Chill";
-    }
-    playSound_work();
-  } else {
-    chillStagesCompleted++;
-    if (stage === "Chill" || stage === "Long Chill") {
-      cycleCount++;
-    }
-    stage = "Work";
-    playSound_chill();
-  }
-  remainingSeconds = durations[stage];
-  updateDisplay();
-  updateCircle(durations[stage] - remainingSeconds, durations[stage]);
-  if (isTimerRunning) {
-    startTimer();
-  }
-  updateCircleColor(stage);
-}
 
-function updateCircleColor(stage) {
-  const progressCirclLine = document.querySelector(".e-c-base");
-  const progressCircle = document.querySelector(".e-c-pointer");
-
-  let color;
-  switch (stage) {
-    case "Work":
-      color = "#d44848"; // Красный
-      break;
-    case "Chill":
-      color = "#3498db"; // Синий
-      break;
-    case "Long Chill":
-      color = "#2c3e50"; // Темно-синий
-      break;
-    default:
-      color = "#d44848"; // Значение по умолчанию
-  }
-  progressCirclLine.style.stroke = color;
-  progressCircle.style.fill = color;
-  progressCircle.style.stroke = color;
-}
-
-function startTimer() {
-  clearInterval(timerInterval);
-  timerInterval = setInterval(decrementTimer, 1000);
-  actionButton.textContent = "Pause";
-  isTimerRunning = true;
-}
-
+// Time changing
 function decrementTimer() {
   remainingSeconds--;
   updateTimerDisplay();
@@ -197,31 +74,179 @@ function decrementTimer() {
   }
 }
 
+// Changing main title
+function cangeTitle() {
+  let displayText;
+  let textColor;
+
+  switch (stage) {
+    case "Work":
+      displayText = "Time to Work!";
+      textColor = "#FF5733";
+      break;
+    case "Chill":
+      displayText = "Take a Short Break!";
+      textColor = "#3498db";
+      break;
+    case "Long Chill":
+      displayText = "Take a Long Break!";
+      textColor = "#2ecc71";
+      break;
+    default:
+      displayText = stage;
+      textColor = "#d44848";
+  }
+
+  stageDisplay.textContent = displayText;
+  stageDisplay.style.color = textColor;
+  cycleCountDisplay.textContent = `Pomodoro cycles: ${cycleCount}`;
+}
+
+// Pouse and resume checker
 function toggleTimer() {
   if (!isTimerRunning) {
-    startTimer();
+    resumeTimer();
   } else {
     pauseTimer();
   }
 }
 
+// Resume time
+function resumeTimer() {
+  changeColors(currentColor);
+  cangeTitle();
+  clearInterval(timerInterval);
+  timerInterval = setInterval(decrementTimer, 1000);
+  actionButton.textContent = "Pause";
+  isTimerRunning = true;
+}
+
+// Pause time
 function pauseTimer() {
+  changeColors("#808080");
+  stageDisplay.textContent = "Time Paused!";
+  stageDisplay.style.color = "#808080";
+  cycleCountDisplay.textContent = `Pomodoro cycles: ${cycleCount}`;
   clearInterval(timerInterval);
   timerInterval = null;
-  actionButton.textContent = "Start";
+  actionButton.textContent = "Continue";
   isTimerRunning = false;
 }
 
+// Restart time
 function restartCurrentSession() {
   clearInterval(timerInterval);
   timerInterval = null;
   isTimerRunning = false;
-  actionButton.textContent = "Start";
+  actionButton.textContent = "Continue";
 
   remainingSeconds = durations[stage];
 
   updateTimerDisplay();
   updateCircle(durations[stage] - remainingSeconds, durations[stage]);
+}
+
+// Audio pause and play
+document.getElementById("musicToggle").addEventListener("click", function () {
+  if (musicPlaying) {
+    audio.pause();
+    musicPlaying = false;
+  } else {
+    audio.play();
+    musicPlaying = true;
+  }
+});
+
+// Music volume
+volumeControl.addEventListener("input", function () {
+  audio.volume = volumeControl.value;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  stage = "Work";
+  remainingSeconds = durations[stage];
+  updateDisplay();
+  updateCircle(durations[stage] - remainingSeconds, durations[stage]);
+  updateCircleColor(stage);
+});
+
+// Upate progress in circle
+function updateCircle(value, timePercent) {
+  var offset = (-length * value) / timePercent;
+  progressBar.style.strokeDashoffset = offset;
+  pointer.style.transform = `rotate(${(360 * value) / timePercent}deg)`;
+}
+
+function switchStage() {
+  if (stage === "Work") {
+    workStagesCompleted++;
+    if (
+      workStagesCompleted % 2 === 0 &&
+      chillStagesCompleted >= longChillFrequency
+    ) {
+      stage = "Long Chill";
+      chillStagesCompleted = 0;
+      playSound_long_chill();
+    } else {
+      stage = "Chill";
+      playSound_chill();
+    }
+  } else {
+    chillStagesCompleted++;
+    if (stage === "Chill" || stage === "Long Chill") {
+      playSound_work();
+      cycleCount++;
+    }
+    stage = "Work";
+  }
+  remainingSeconds = durations[stage];
+  updateDisplay();
+  updateCircle(durations[stage] - remainingSeconds, durations[stage]);
+  if (isTimerRunning) {
+    resumeTimer();
+  }
+  updateCircleColor(stage);
+}
+
+function updateCircleColor(stage) {
+  let color;
+
+  switch (stage) {
+    case "Work":
+      color = "#FF5733";
+      break;
+    case "Chill":
+      color = "#3498db";
+      break;
+    case "Long Chill":
+      color = "#2ecc71";
+      break;
+    default:
+      color = "#900C3F";
+  }
+  currentColor = color;
+  changeColors(color);
+}
+
+function changeColors(color) {
+  const progressCirclLine = document.querySelector(".e-c-base");
+  const progressCircle = document.querySelector(".e-c-pointer");
+  const button = document.querySelector(".button-start");
+  const restartButton = document.getElementById("restartButton");
+  const settingsButton = document.getElementById("settingsButton");
+  const musicToggle = document.getElementById("musicToggle");
+
+  progressCirclLine.style.stroke = color;
+  progressCircle.style.fill = color;
+  progressCircle.style.stroke = color;
+  button.style.background = color;
+  restartButton.style.background = color;
+  settingsButton.style.background = color;
+  musicToggle.style.background = color;
+}
+
+function playSound_long_chill() {
+  soundAlertChillLong.play();
 }
 
 function playSound_work() {
@@ -232,11 +257,7 @@ function playSound_chill() {
   soundAlertChill.play();
 }
 
-const settingsModal = document.getElementById("settingsModal");
-const settingsButton = document.getElementById("settingsButton");
-const span = document.getElementsByClassName("close")[0];
-const applySettingsButton = document.getElementById("applySettings");
-
+// Setting displaying
 settingsButton.onclick = function () {
   settingsModal.style.display = "block";
 };
@@ -250,6 +271,8 @@ window.onclick = function (event) {
     settingsModal.style.display = "none";
   }
 };
+
+// Setting valide input checking
 document
   .getElementById("workDuration")
   .addEventListener("input", validateInput);
@@ -259,7 +282,11 @@ document
 document
   .getElementById("longChillDuration")
   .addEventListener("input", validateInput);
+document
+  .getElementById("longChillFrequency")
+  .addEventListener("input", validateInput);
 
+// Checking input validation in settings
 function validateInput() {
   const workDuration = parseInt(
     document.getElementById("workDuration").value,
@@ -273,6 +300,10 @@ function validateInput() {
     document.getElementById("longChillDuration").value,
     10,
   );
+  const longChillFrequencyInput = parseInt(
+    document.getElementById("longChillFrequency").value,
+    10,
+  );
 
   if (
     workDuration > 0 &&
@@ -280,7 +311,9 @@ function validateInput() {
     longChillDuration > 0 &&
     workDuration < 1000 &&
     chillDuration < 1000 &&
-    longChillDuration < 1000
+    longChillDuration < 1000 &&
+    longChillFrequencyInput >= 1 &&
+    longChillFrequencyInput <= 5
   ) {
     document.getElementById("applySettings").disabled = false;
   } else {
@@ -288,8 +321,7 @@ function validateInput() {
   }
 }
 
-validateInput();
-
+// Setting apply functions
 applySettingsButton.onclick = function () {
   const workDurationInput =
     parseInt(document.getElementById("workDuration").value, 10) * 60;
@@ -297,57 +329,21 @@ applySettingsButton.onclick = function () {
     parseInt(document.getElementById("chillDuration").value, 10) * 60;
   const longChillDurationInput =
     parseInt(document.getElementById("longChillDuration").value, 10) * 60;
+  const longChillFrequencyInput = parseInt(
+    document.getElementById("longChillFrequency").value,
+    10,
+  );
 
-  if (
-    workDurationInput > 0 &&
-    chillDurationInput > 0 &&
-    longChillDurationInput > 0
-  ) {
-    durations["Work"] = workDurationInput;
-    durations["Chill"] = chillDurationInput;
-    durations["Long Chill"] = longChillDurationInput;
+  durations["Work"] = workDurationInput;
+  durations["Chill"] = chillDurationInput;
+  durations["Long Chill"] = longChillDurationInput;
+  longChillFrequency = longChillFrequencyInput;
 
-    remainingSeconds = durations[stage];
-    updateTimerDisplay();
-    updateCircle(durations[stage] - remainingSeconds, durations[stage]);
-    settingsModal.style.display = "none";
-  } else {
-    alert("Please enter positive numbers greater than zero for all durations.");
-  }
-};
-
-function updateDisplay() {
-  let displayText;
-  let textColor;
-
-  switch (stage) {
-    case "Work":
-      displayText = "Time to Work!";
-      textColor = "#6B70B0";
-      break;
-    case "Chill":
-      displayText = "Chill Out!";
-      textColor = "#3498db";
-      break;
-    case "Long Chill":
-      displayText = "Long Break!";
-      textColor = "#2ecc71";
-      break;
-    default:
-      displayText = stage;
-      textColor = "#d44848";
-  }
-
-  stageDisplay.textContent = displayText;
-  stageDisplay.style.color = textColor;
-  cycleCountDisplay.textContent = `Cycles: ${cycleCount}`;
-  updateTimerDisplay();
   remainingSeconds = durations[stage];
-}
-
-function initializeTimer() {
-  updateDisplay();
-}
+  updateTimerDisplay();
+  updateCircle(durations[stage] - remainingSeconds, durations[stage]);
+  settingsModal.style.display = "none";
+};
 
 actionButton.addEventListener("click", toggleTimer);
 restartButton.addEventListener("click", restartCurrentSession);
